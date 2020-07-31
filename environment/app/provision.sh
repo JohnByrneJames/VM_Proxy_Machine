82b3eb5af126c90b8ced971d4940f213d60f3df1
John Byrne <neardem1@gmail.com>
2020-07-31 14:32:18 +0100

    Pushing to Init

---
+++ environment/app/provision.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Update the sources list
+sudo apt-get update -y
+
+# upgrade any packages available
+sudo apt-get upgrade -y
+
+# install nginx
+sudo apt-get install nginx -y
+
+# install git
+sudo apt-get install git -y
+
+# install nodejs
+sudo apt-get install python-software-properties
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+sudo apt-get install nodejs -y
+
+# install pm2
+sudo npm install pm2 -g
